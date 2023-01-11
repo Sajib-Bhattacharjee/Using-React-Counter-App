7bc16f3ae071b051aa17a34bc17d1e937aacdfb9
Sajib Bhattacharjee <86997775+Sajib-Bhattacharjee@users.noreply.github.com>
2023-01-11 22:20:44 +0600

    Uploaded Successfully Everything.

---
+++ README.md
@@ -0,0 +1,7 @@
+### `A Simple Counter App`
+
+```
+Using Simple Knowledge of Basic useState Hook I've created it.
+```
+
+> #### Thanks a lot for Visiting...
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CounterApp from "./COMPONENTS/counter.js";
+
+export default function App() {
+  return (
+    <div>
+      <CounterApp />
+    </div>
+  );
+}
